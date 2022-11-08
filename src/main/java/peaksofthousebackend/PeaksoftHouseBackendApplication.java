c26399e9a2befe9cbeead5ae008aa8cf8fa671d7
BeksultanDev <beksultancs@gmail.com>
2022-11-09 00:08:23 +0600

    Initial commit

---
+++ src/main/java/peaksofthousebackend/PeaksoftHouseBackendApplication.java
@@ -0,0 +1,13 @@
+package peaksofthousebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeaksoftHouseBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeaksoftHouseBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/peaksofthousebackend/PeaksoftHouseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package peaksofthousebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeaksoftHouseBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
